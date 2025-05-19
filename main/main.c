1fe78e541f7667889c023d1a8a9e1f41dab98df6
ckj11 <ckjahn@proton.me>
2025-05-18 20:33:54 -0400

    basic wireless access implemented and tested. Shows on router

--- main/main.c
+++ main/main.c
@@ -14,7 +14,8 @@
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_netif.h"
-#include "../wifiStation/wifiStation.h"
+#include "wifiStation.h"
+#include "nvs_flash.h"
 
 #define LOCAL_ESP_WIFI_SSID         CONFIG_ESP_WIFI_SSID
 #define LOCAL_ESP_WIFI_PASS         CONFIG_ESP_WIFI_PASSWORD
@@ -22,11 +23,26 @@
 
 void app_main(void)
 {
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     
+    init_wifi_station();
+
 
     printf("Restarting now.\n");
+    for (int i = 10; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
     fflush(stdout);
     esp_restart();
 }
